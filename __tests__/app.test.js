82028288d34325b3405ecaa8fbe1ce02a562abce
AngelDOrtiz <aortizghs@gmail.com>
2021-05-12 16:24:25 -0700

    first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,33 @@
-import app from '../lib/app.js';
-import supertest from 'supertest';
 
-const request = supertest(app);
-
-describe('API Routes', () => {
+import { cityData } from '../data/cities';
+import { formatCities } from '../lib/munge-utils';
 
 
+
+describe('API City Munging', () => {
+
+  const expectedCities = [
+    {
+      'formatted_query': 'Reno, Washoe County, Nevada, USA',
+      'latitude': '39.5261206',
+      'longitude': '-119.8126581'
+    },
+    {
+      'formatted_query': 'Branson, Taney County, Missouri, USA',
+      'latitude': '36.6411357',
+      'longitude': '-93.2175285'
+    },
+    {
+      'formatted_query': 'Hell, Putnam Township, Livingston County, Michigan, 48169, USA',
+      'latitude': '42.4347571',
+      'longitude': '-83.9849477'
+    }
+  ];
+
+  it('munges city data', async () => {
+    const output = formatCities(cityData);
+
+    expect(output).toEqual(expectedCities);
+  });
 
 });
---
+++ data/cities.js
@@ -0,0 +1,60 @@
+export const cityData = 
+  [
+    {
+      'place_id': '297121243',
+      'licence': 'https://locationiq.com/attribution',
+      'osm_type': 'relation',
+      'osm_id': '170120',
+      'boundingbox': [
+        '39.392426',
+        '39.723436',
+        '-120.0023728',
+        '-119.699345'
+      ],
+      'lat': '39.5261206',
+      'lon': '-119.8126581',
+      'display_name': 'Reno, Washoe County, Nevada, USA',
+      'class': 'place',
+      'type': 'city',
+      'importance': 0.673480172341805,
+      'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+    },
+    {
+      'place_id': '235310291',
+      'licence': 'https://locationiq.com/attribution',
+      'osm_type': 'relation',
+      'osm_id': '140500',
+      'boundingbox': [
+        '36.594325',
+        '36.700907',
+        '-93.31472',
+        '-93.211345'
+      ],
+      'lat': '36.6411357',
+      'lon': '-93.2175285',
+      'display_name': 'Branson, Taney County, Missouri, USA',
+      'class': 'place',
+      'type': 'city',
+      'importance': 0.551140838706628,
+      'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+    },
+    {
+      'place_id': '439632',
+      'licence': 'https://locationiq.com/attribution',
+      'osm_type': 'node',
+      'osm_id': '154309316',
+      'boundingbox': [
+        '42.4147571',
+        '42.4547571',
+        '-84.0049477',
+        '-83.9649477'
+      ],
+      'lat': '42.4347571',
+      'lon': '-83.9849477',
+      'display_name': 'Hell, Putnam Township, Livingston County, Michigan, 48169, USA',
+      'class': 'place',
+      'type': 'hamlet',
+      'importance': 0.46925190422504404,
+      'icon': 'https://locationiq.org/static/images/mapicons/poi_place_village.p.20.png'
+    },
+  ];
